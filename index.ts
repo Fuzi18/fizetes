523e23fce97c3e02cc7c6ed83f848498b2c558c9
Fuzi18 <fuzesi.t2003@gmail.com>
2022-10-26 14:24:48 +0200

    s

--- index.ts
+++
@@ -1,8 +0,0 @@
-let nev=/[a-zA-Z]{1,}/;
-let orszag=/[a-zA-Z]{1,}/;
-let varos=/[a-zA-Z]{1,}/;
-let utca=/[a-zA-Z 0-9]{1,}/;
-let iranyitoSzam=/[A-Z 0-9]/;
-let bankkartyaSzam=/[0-9]{4,4}+[0-9]{4,4}+[0-9]{4,4}+[0-9]{4,4}/;
-let ellenorzoKod=/[0-9]{3,3}/;
-let bankkartyaraIrtNev=/[a-zA-Z ]/;
