b0e59fece7d4c0ea57acd43d5c99d97df0ac2e24
Fuzi18 <fuzesi.t2003@gmail.com>
2022-10-27 12:10:46 +0200

    aha

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let nev = /[a-zA-Z]{1,}/;
+let orszag = /[a-zA-Z]{1,}/;
+let varos = /[a-zA-Z]{1,}/;
+let utca = /[a-zA-Z 0-9]{1,}/;
+let iranyitoSzam = /[A-Z 0-9]/;
+let bankkartyaSzam = /[0-9]{4,4}-[0-9]{4,4}-[0-9]{4,4}-[0-9]{4,4}/;
+let ellenorzoKod = /[0-9]{3,3}/;
+let bankkartyaraIrtNev = /[a-zA-Z ]/;
+document.getElementById("gomb").addEventListener("click", () => {
+    let abankartya = document.getElementById("kartyaszam").value;
+    console.log(bankkartyaSzam.test(abankartya));
+});
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ let bankkartyaSzam=/[0-9]{4,4}-[0-9]{4,4}-[0-9]{4,4}-[0-9]{4,4}/;
 let ellenorzoKod=/[0-9]{3,3}/;
 let bankkartyaraIrtNev=/[a-zA-Z ]/;
 
-document.getElementById("gomb").addEventListener("click",()=>{
-    let abankartya=document.getElementById("kartyaszam").value;
+document.getElementById("gomb")!.addEventListener("click",()=>{
+    let abankartya=(document.getElementById("kartyaszam")as HTMLInputElement).value;
     console.log(bankkartyaSzam.test(abankartya));
 
 })
